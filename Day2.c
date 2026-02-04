7c6d765fc40e787d2ab2a10670e2907e132851d5
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2026-02-05 02:00:10 +0530

    Day 2 Done

--- Day2.c
+++ Day2.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
-
-int main() {
-    int n, pos, i;
-    int arr[100];
-    
-    scanf("%d", &n);
-    
-    for(i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    
-    scanf("%d", &pos);
-    
-    for(i = pos - 1; i < n - 1; i++) {
-        arr[i] = arr[i + 1];
-    }
-    
-    for(i = 0; i < n - 1; i++) {
-        printf("%d ", arr[i]);
-    }
-    
-    return 0;
-}
+ #include <stdio.h>
+ 
+ int main(){
+ 
+            int a[100],i,n,pos;
+            printf("Enter the Size:\n");
+            scanf("%d",&n);
+            printf("Enter the Elements:\n");
+            for(i=0;i<n;i++){
+                        scanf("%d",&a[i]);
+            }
+            printf("Enter the Position from which you have Delete the Element:\n");
+            scanf("%d",&pos);
+            for(i=pos-1;i<n-1;i++){
+                        a[i]=a[i+1];
+            }
+            n--;
+            for(i=0;i<n;i++){
+                        printf("%d",a[i]);
+            }
+            return 0;
+ }
