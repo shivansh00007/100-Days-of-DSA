740799b053ceb19922d3910d6992a0dfd172d766
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2026-02-05 00:41:24 +0530

    Day 1 Done

--- Day1.c
+++ Day1.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
-int main() {
-    int n, pos, x, i;
-    int arr[100];
-    
-    scanf("%d", &n);
-    
-    for(i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    
-    scanf("%d", &pos);
-    scanf("%d", &x);
-    
-    for(i = n; i >= pos; i--) {
-        arr[i] = arr[i - 1];
-    }
-    
-    arr[pos - 1] = x;
-    
-    for(i = 0; i <= n; i++) {
-        printf("%d ", arr[i]);
-    }
-    
-    return 0;
+int main(){
+            int n,i,a[100],num,pos;
+            printf("Enter the Size:\n");
+            scanf("%d",&n);
+            printf("Enter the Elements:\n");
+            for(i=0;i<n;i++){
+                        scanf("%d",&a[i]);
+            }
+            printf("Enter the number want to insert:\n");
+            scanf("%d",&num);
+            printf("Enter the Position:\n");
+            scanf("%d",&pos);
+            for(i=n-1;i>=pos-1;i--){
+                        a[i+1]=a[i];
+                        
+            }
+            a[pos-1]=num;
+            n++;
+            for(i=0;i<n;i++){
+                        printf("%d",a[i]);
+            }
+            return 0;
 }
