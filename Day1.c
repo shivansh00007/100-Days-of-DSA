2dde9e5c2b2f71196d4b344cac87a2a2648db99d
shivansh00007 <shivansh.upadhyay.uk@gmail.com>
2026-02-05 09:06:36 +0530

    Day 2 Done

--- Day1.c
+++ Day1.c
@@ -4,6 +4,10 @@ int main(){
             int n,i,a[100],num,pos;
             printf("Enter the Size:\n");
             scanf("%d",&n);
+            if (n >= 100) {
+            printf("Overflow! Array is full.\n");
+            return 0;
+            }
             printf("Enter the Elements:\n");
             for(i=0;i<n;i++){
                         scanf("%d",&a[i]);
@@ -12,6 +16,11 @@ int main(){
             scanf("%d",&num);
             printf("Enter the Position:\n");
             scanf("%d",&pos);
+
+            if (pos < 1 || pos > n + 1) {
+            printf("Invalid position\n");
+            return 0;
+            }
             for(i=n-1;i>=pos-1;i--){
                         a[i+1]=a[i];
                         
--- Day2.c
+++ Day2.c
@@ -5,12 +5,20 @@
             int a[100],i,n,pos;
             printf("Enter the Size:\n");
             scanf("%d",&n);
+            if (n == 0) {
+            printf("Underflow! Array is empty\n");
+            return 0;
+            }
             printf("Enter the Elements:\n");
             for(i=0;i<n;i++){
                         scanf("%d",&a[i]);
             }
             printf("Enter the Position from which you have Delete the Element:\n");
             scanf("%d",&pos);
+            if (pos < 1 || pos > n) {
+            printf("Invalid position\n");
+            return 0;
+            }
             for(i=pos-1;i<n-1;i++){
                         a[i]=a[i+1];
             }
